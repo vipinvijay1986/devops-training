503af754a04f24b70085cf197dd98195bdb9fb25
vipin <vipinpublicdomain@gmail.com>
2019-09-11 22:02:28 +0530

    variable file

--- terraform/ey-uat/variables.tf
+++ terraform/ey-uat/variables.tf
@@ -13,7 +13,9 @@ variable "db_password" {
 }
 
 variable "client_id" {
+    default = "1ae4bed0-01aa-4e4c-bcf6-3dc3611817e5"
 }
 
 variable "client_secret" {
+    default = "3868bc05-c4b6-42a7-a6b9-628788c43200"
 }
